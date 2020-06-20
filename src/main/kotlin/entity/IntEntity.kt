e3a5e89a9362226157442dcad61c478e6326e60e
0918nobita <nobita.0918@gmail.com>
2020-06-20 13:56:45 +0900

    :building_construction: IntEntity.add が Location を引数に取らないようにした

--- src/main/kotlin/entity/IntEntity.kt
+++ src/main/kotlin/entity/IntEntity.kt
@@ -5,17 +5,15 @@ import arrow.core.Left
 import arrow.core.None
 import arrow.core.Option
 import arrow.core.Right
-import arrow.core.or
 import vision.kodai.xemime.ast.Location
 
 class IntEntity(override val location: Option<Location>, private val value: Int) : Entity {
     override fun toString() = "IntEntity[$value]"
 
-    fun add(rhs: Entity, exprLoc: Option<Location> = None): Either<String, IntEntity> {
+    fun add(rhs: Entity): Either<String, IntEntity> =
         if (rhs !is IntEntity) {
-            val locMsg = (exprLoc or location).fold({ ".." }, { it.toString() })
-            return Left("TypeError ($locMsg)")
+            Left("TypeError")
+        } else {
+            Right(IntEntity(None, this.value + rhs.value))
         }
-        return Right(IntEntity(None, this.value + rhs.value))
-    }
 }
